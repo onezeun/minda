718bd6c61355020e78a9c76774358fa015233d7b
onezeun <onezeun@gmail.com>
2022-10-17 10:30:48 +0900

    refactor: 제이쿼리로 변경

--- js/signup.js
+++ js/signup.js
@@ -1,158 +1,164 @@
-$(document).ready(function() {
-  var email = document.getElementById('email');
-  var pwd = document.getElementById('pwd');
-  var repwd = document.getElementById('repwd');
-  var name = document.getElementById('name');
-  var nickname = document.getElementById('nickname');
-  var mobile = document.getElementById('mobile');
-  var chackbox01 = document.getElementById('chackbox01');
-  var chackbox02 = document.getElementById('chackbox02');
-
-  var err_email = document.getElementById('err_email');
-  var err_pwd = document.getElementById('err_pwd');
-  var err_repwd = document.getElementById('err_repwd');
-  var err_name = document.getElementById('err_name');
-  var err_nickname = document.getElementById('err_nickname');
-  var err_mobile = document.getElementById('err_mobile');
-  var err_apply = document.getElementById('err_apply');
-
+$(document).ready(function () {
   /* 정규식 */
   var regEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-  var regPwd = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+  var regPwd =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
   var regMobile = /^[0-9]+$/;
 
   const emailCheck = () => {
-    if (!email.value) {
-      err_email.style.display = 'block';
-      email.setAttribute('class', "err_email_input");
-      err_email.innerHTML = '이메일을 입력해주세요';
-      email.focus();
+    if (!$('#email').val()) {
+      $('#email').addClass('err_email_input').focus();
+      $('#email').removeClass('email_input');
+      $('#err_email').css({ display: 'block' }).text('이메일을 입력해주세요');
       return false;
-    } else if(!regEmail.test(email.value)) { 
-      err_email.style.display = 'block';
-      email.setAttribute('class', "err_email_input");
-      err_email.innerHTML = '올바른 이메일 형식이 아닙니다';
-      email.focus();
+    } else if (!regEmail.test($('#email').val())) {
+      $('#email').addClass('err_email_input').focus();
+      $('#email').removeClass('email_input');
+      $('#err_email')
+        .css({ display: 'block' })
+        .text('올바른 이메일 형식이 아닙니다');
       return false;
     } else {
-      email.removeAttribute('class', "err_email_input");
-      email.setAttribute('class', "email_input");
-      err_email.style.display = 'none';
+      $('#email').addClass('email_input');
+      $('#email').removeClass('err_email_input');
+      $('#err_email').css({ display: 'none' });
       return true;
     }
   };
-  email.addEventListener('keyup', emailCheck);
+
+  $('#email').on('keyup', function () {
+    emailCheck();
+  });
 
   const pwdCheck = () => {
-    if (!pwd.value) {
-      err_pwd.style.display = 'block';
-      pwd.setAttribute('class', "err_input");
-      err_pwd.innerHTML = '비밀번호를 입력해주세요';
-      pwd.focus();
+    if (!$('#pwd').val()) {
+      $('#pwd').addClass('err_input').focus();
+      $('#pwd').removeClass('signup_input');
+      $('#err_pwd').css({ display: 'block' }).text('비밀번호를 입력해주세요');
       return false;
-    } else if(!regPwd.test(pwd.value)) {
-      err_pwd.style.display = 'block';
-      pwd.setAttribute('class', "err_input");
-      err_pwd.innerHTML = '문자/숫자/특수문자 포함 8자 이상 입력해주세요';
-      pwd.focus();
+    } else if (!regPwd.test($('#pwd').val())) {
+      $('#pwd').addClass('err_input').focus();
+      $('#pwd').removeClass('signup_input');
+      $('#err_pwd')
+        .css({ display: 'block' })
+        .text('문자/숫자/특수문자 포함 8자 이상 입력해주세요');
       return false;
     } else {
-      err_pwd.style.display = 'none';
-      pwd.removeAttribute('class', "err_input");
-      pwd.setAttribute('class', "signup_input");
+      $('#pwd').addClass('signup_input');
+      $('#pwd').removeClass('err_input');
+      $('#err_pwd').css({ display: 'none' });
       return true;
     }
   };
-  pwd.addEventListener('keyup', pwdCheck);
+
+  $('#pwd').on('keyup', function () {
+    pwdCheck();
+  });
 
   const repwdCheck = () => {
-    if (!repwd.value) {
-      err_repwd.style.display = 'block';
-      repwd.setAttribute('class', "err_input");
-      err_repwd.innerHTML = '비밀번호 확인을 입력해주세요';
-      repwd.focus();
+    if (!$('#repwd').val()) {
+      $('#repwd').addClass('err_input').focus();
+      $('#repwd').removeClass('signup_input');
+      $('#err_repwd').css({ display: 'block' }).text('비밀번호 확인을 입력해주세요');
       return false;
-    } else if(pwd.value != repwd.value) {
-      err_repwd.style.display = 'block';
-      repwd.setAttribute('class', "err_input");
-      err_repwd.innerHTML = '비밀번호가 일치하지 않습니다.';
+    } else if ($("#pwd").val() != $("#repwd").val()) {
+      $('#repwd').addClass('err_input').focus();
+      $('#repwd').removeClass('signup_input');
+      $('#err_repwd')
+        .css({ display: 'block' })
+        .text('비밀번호가 일치하지 않습니다');
       return false;
     } else {
-      err_repwd.style.display = 'none';
-      repwd.removeAttribute('class', "err_input");
-      repwd.setAttribute('class', "signup_input");
+      $('#repwd').addClass('signup_input');
+      $('#repwd').removeClass('err_input');
+      $('#err_repwd').css({ display: 'none' });
       return true;
     }
   };
-  repwd.addEventListener('keyup', repwdCheck);
+
+  $('#repwd').on('keyup', function () {
+    repwdCheck();
+  });
 
   const nameCheck = () => {
-    var name_len = name.value.length
-    if (!name.value) {
-      err_name.style.display = 'block';
-      name.setAttribute('class', "err_input");
-      err_name.innerHTML = '이름을 입력해주세요';
-      name.focus();
+    var name_len = $("#name").val().length;
+
+    if (!$('#name').val()) {
+      $('#name').addClass('err_input').focus();
+      $('#name').removeClass('signup_input');
+      $('#err_name').css({ display: 'block' }).text('비밀번호 확인을 입력해주세요');
       return false;
-    } else if(name_len < 2) {
-      err_name.style.display = 'block';
-      name.setAttribute('class', "err_input");
-      err_name.innerHTML = '두 글자 이상 입력해주세요';
-      name.focus();
+    } else if (name_len < 2) {
+      $('#name').addClass('err_input').focus();
+      $('#name').removeClass('signup_input');
+      $('#err_name')
+        .css({ display: 'block' })
+        .text('두 글자 이상 입력해주세요');
       return false;
     } else {
-      err_name.style.display = 'none';
-      name.removeAttribute('class', "err_input");
-      name.setAttribute('class', "signup_input");
+      $('#name').addClass('signup_input');
+      $('#name').removeClass('err_input');
+      $('#err_name').css({ display: 'none' });
       return true;
     }
   };
-  name.addEventListener('keyup', nameCheck);
+
+  $('#name').on('keyup', function () {
+    nameCheck();
+  });
+
 
   const nicknameCheck = () => {
-    var nickname_len = nickname.value.length
-    if (!nickname.value) {
-      err_nickname.style.display = 'block';
-      nickname.setAttribute('class', "err_input");
-      err_nickname.innerHTML = '닉네임을 입력해주세요';
-      nickname.focus();
+    var nickname_len = $("#nickname").val().length;
+
+    if (!$('#nickname').val()) {
+      $('#nickname').addClass('err_input').focus();
+      $('#nickname').removeClass('signup_input');
+      $('#err_nickname').css({ display: 'block' }).text('비밀번호 확인을 입력해주세요');
       return false;
-    } else if(nickname_len < 2) {
-      err_nickname.style.display = 'block';
-      nickname.setAttribute('class', "err_input");
-      err_nickname.innerHTML = '두 글자 이상 입력해주세요';
-      nickname.focus();
+    } else if (nickname_len < 2) {
+      $('#nickname').addClass('err_input').focus();
+      $('#nickname').removeClass('signup_input');
+      $('#err_nickname')
+        .css({ display: 'block' })
+        .text('두 글자 이상 입력해주세요');
       return false;
     } else {
-      err_nickname.style.display = 'none';
-      nickname.removeAttribute('class', "err_input");
-      nickname.setAttribute('class', "signup_input");
+      $('#nickname').addClass('signup_input');
+      $('#nickname').removeClass('err_input');
+      $('#err_nickname').css({ display: 'none' });
       return true;
     }
   };
-  nickname.addEventListener('keyup', nicknameCheck);
+
+  $('#nickname').on('keyup', function () {
+    nicknameCheck();
+  });
 
   const mobileCheck = () => {
-    if (!mobile.value) {
-      err_mobile.style.display = 'block';
-      mobile.setAttribute('class', "err_input");
-      err_mobile.innerHTML = '휴대폰번호를 입력해주세요';
-      mobile.focus();
+    if (!$('#mobile').val()) {
+      $('#mobile').addClass('err_input').focus();
+      $('#mobile').removeClass('signup_input');
+      $('#err_mobile').css({ display: 'block' }).text('휴대폰 번호를 입력해주세요');
       return false;
-    } else if(!regMobile.test(mobile.value)) {
-      err_mobile.style.display = 'block';
-      mobile.setAttribute('class', "err_input");
-      err_mobile.innerHTML = '"-"없이 숫자만 입력해주세요';
-      mobile.focus();
+    } else if (!regMobile.test($('#mobile').val())) {
+      $('#mobile').addClass('err_input').focus();
+      $('#mobile').removeClass('signup_input');
+      $('#err_mobile')
+        .css({ display: 'block' })
+        .text('"-"없이 숫자만 입력해주세요');
       return false;
     } else {
-      err_mobile.style.display = 'none';
-      mobile.removeAttribute('class', "err_input");
-      mobile.setAttribute('class', "signup_input");
+      $('#mobile').addClass('signup_input');
+      $('#mobile').removeClass('err_input');
+      $('#err_mobile').css({ display: 'none' });
       return true;
     }
   };
-  mobile.addEventListener('input', mobileCheck);
+
+  $('#mobile').on('input', function () {
+    mobileCheck();
+  });
 
   // const applyCheck = () => {
   //   if (!chackbox01.checked) {
@@ -168,9 +174,15 @@ $(document).ready(function() {
   // chackbox01.addEventListener('change', applyCheck);
   // chackbox02.addEventListener('change', applyCheck);
 
-
-  $("#signup_btn").on("click", function() {
-    if(!emailCheck() || !pwdCheck() || !repwdCheck() || !nameCheck() || !nicknameCheck() || !mobileCheck()) {
+  $('#signup_btn').on('click', function () {
+    if (
+      !emailCheck() ||
+      !pwdCheck() ||
+      !repwdCheck() ||
+      !nameCheck() ||
+      !nicknameCheck() ||
+      !mobileCheck()
+    ) {
       emailCheck();
       pwdCheck();
       repwdCheck();
@@ -178,6 +190,6 @@ $(document).ready(function() {
       nicknameCheck();
       mobileCheck();
       return false;
-    }else $("#signup_form").submit();
-  })
+    } else $('#signup_form').submit();
+  });
 });

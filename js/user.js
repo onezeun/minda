9d945a86829a3a9562897280b5fae311d9588aad
onezeun <onezeun@gmail.com>
2022-10-21 12:59:04 +0900

    feat: src 값 넘김,  프로필 사진 미리보기 구현

--- js/user.js
+++ js/user.js
@@ -33,6 +33,37 @@ $(document).ready(function () {
     $('.default_cont').show();
   });
 
+  /* 프로필 사진 */
+  $('#uplode_btn').on('click', function(e){
+    e.preventDefault();
+    $('#img_input').click();
+  })
+
+  $('#remove_btn').on('click', function(e){
+    e.preventDefault();
+    $('#profile_photo_edit').attr('src', "./images/profile.png")
+  })
+
+  /* 미리보기 */
+  $('#img_input').change(function () {
+    setImageFromFile(this, '#profile_photo_edit');
+  });
+
+  const setImageFromFile = (input, expression) => {
+    if (input.files && input.files[0]) {  //파일이 있으면
+      var reader = new FileReader();      //파일을 읽는 객체 생성
+      reader.onload = function (e) {
+        //파일을 읽기에 성공하면 e 변수로 접근
+        //이미지 Tag의 SRC속성에 읽어들인 File내용을 지정
+        //(아래 코드에서 읽어들인 dataURL형식)
+        $(expression).attr('src', e.target.result); // html태그 중 id 가 empImg 인 태그에 src 속성 값을 읽은 파일로 변경 ( src = "파일에서 읽은 값 즉 파일")
+        // console.log(e.target.result);
+      };
+      // File내용을 읽어 dataURL형식의 문자열로 저장
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
   /* 유효성 검사 */
   var regMobile = /^[0-9]+$/;
 
@@ -129,11 +160,15 @@ $(document).ready(function () {
       mobileCheck();
       return false;
     } else {
-      $('#user_form').submit();
       $('.edit_cont').hide();
       $('.default_cont').show();
       $('.btn').hide();
 
+      if(!$('#profile_photo_edit').attr('src') == '') {
+        var new_img = $('#profile_photo_edit').attr('src')
+        $('#profile_photo_save').attr('src', new_img)
+      }
+      
       if ($('#user_name_input').val() != $('.user_name_val')) {
         $('.user_name_val').text($('#user_name_input').val());
       }
@@ -151,6 +186,8 @@ $(document).ready(function () {
       } else {
         $('.mk_txt').text('수신거부');
       }
+      
+     // $('#user_form').submit();
     }
   });
 });

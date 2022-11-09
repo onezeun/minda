610bdc3c9ab389da29fc173c9d617750ede2ea7f
onezeun <onezeun@gmail.com>
2022-11-09 23:03:29 +0900

    feat: 파트너 홈페이지 푸터 분리

--- js/includ.js
+++ js/includ.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   $('#header-include').load('http://localhost/KDT-1st-project-minda/inc/header.php');
   $('#footer-include').load('http://localhost/KDT-1st-project-minda/inc/footer.html');
+  $('#partner-footer-include').load('http://localhost/KDT-1st-project-minda/inc/partner_footer.html');
 });

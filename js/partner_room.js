8f2293ab083385ba6d718f002f1f2ca1ba821fce
onezeun <onezeun@gmail.com>
2022-11-10 11:25:32 +0900

    feat: 숙소 소개, 객실 등록  팝업 UI

--- js/partner_room.js
+++ js/partner_room.js
@@ -8,4 +8,37 @@ $(document).ready(function () {
     prevArrow: $('#room_btn1'),
     nextArrow: $('#room_btn2'),
   });
+
+  /* 숙소 소개 팝업 */
+  $(".ldg_info_btn").on("click", function (e) {
+    e.preventDefault();
+
+    $(".ldg_info_modal_bg").fadeTo("fast", 1);
+    $("body").addClass("scrollLock");
+  });
+
+  $("#ldg_pop_cancel").on("click", function () {
+    $(".ldg_info_modal_bg").hide();
+    $("body").removeClass("scrollLock");
+  });
+
+  $('#info_img_btn').on('click', function() {
+    $('#ldg_info_btn').text("등록 내용 확인").css({color : "#eeb72f"})
+    $(".ldg_info_modal_bg").hide();
+    $("body").removeClass("scrollLock");
+  })
+
+  /* 객실 등록 팝업 */
+  $("#room_btn").on("click", function (e) {
+    e.preventDefault();
+
+    $(".room_modal_bg").fadeTo("fast", 1);
+    $("body").addClass("scrollLock");
+  });
+
+  $("#room_pop_cancel").on("click", function () {
+    $(".room_modal_bg").hide();
+    $("body").removeClass("scrollLock");
+  });
+
 });

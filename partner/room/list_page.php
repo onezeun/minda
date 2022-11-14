d149ed08cfe5d749021f0d6e2cc05eadd8dc621c
onezeun <onezeun@gmail.com>
2022-11-14 18:25:14 +0900

    feat: 숙소 리스트 조회 DB 연동

--- partner/room/list_page.php
+++ partner/room/list_page.php
@@ -1,5 +1,51 @@
 <?php
   include "../inc/session.php";
+
+  // DB 연결
+  include "../inc/dbcon.php";
+
+  // 쿼리 작성
+  $sql = "select * from lodging where p_idx='$sp_idx';";
+
+  // 쿼리 전송
+  $result = mysqli_query($dbcon, $sql);
+
+  // 전체 데이터 가져오기
+  $total = mysqli_num_rows($result);
+
+  // paging : 한 페이지 당 보여질 목록 수
+  $list_num = 3;
+
+  // paging : 한 블럭 당 페이지 수
+  $page_num = 5;
+
+  // paging : 현재 페이지
+  $page = isset($_GET["page"]) ? $_GET["page"] : 1 ;
+
+  // paging : 전체 페이지 수 = 전체 데이터 / 페이지 당 목록 수,  ceil : 올림값, floor : 내림값, round : 반올림
+  $total_page = ceil($total / $list_num);
+  // echo "전체 페이지수 : ".$total_page;
+  // exit;
+
+  // paging : 전체 블럭 수 = 전체 페이지 수 / 블럭 당 페이지 수
+  $total_block = ceil($total_page / $page_num);
+
+  // paging : 현재 블럭 번호 = 현재 페이지 번호 / 블럭 당 페이지 수
+  $now_block = ceil($page / $page_num);
+
+  // paging : 블럭 당 시작 페이지 번호 = (해당 글의 블럭 번호 - 1) * 블럭 당 페이지 수 + 1
+  $s_pageNum = ($now_block - 1) * $page_num + 1;
+  if($s_pageNum <= 0){
+      $s_pageNum = 1;
+  };
+
+  // paging : 블럭 당 마지막 페이지 번호 = 현재 블럭 번호 * 블럭 당 페이지 수
+  $e_pageNum = $now_block * $page_num;
+  // 블럭 당 마지막 페이지 번호가 전체 페이지 수를 넘지 않도록
+  if($e_pageNum > $total_page){
+      $e_pageNum = $total_page;
+  };
+
 ?>
 
 <!DOCTYPE html>
@@ -53,18 +99,36 @@
       </div>
 
       <div class="cont">
-        <p class="list_title">총 <span>1</span>개의 숙소가 등록되어있습니다.</p>
+        <p class="list_title">총 <span><?php echo $total; ?></span>개의 숙소가 등록되어있습니다.</p>
         <a href="new_room_page.php" class="new_ldg btn_hover">신규 등록</a>
+        <?php
+          // paging : 해당 페이지의 글 시작 번호 = (현재 페이지 번호 - 1) * 페이지 당 보여질 목록 수
+          $start = ($page - 1) * $list_num;
+
+          // paging : 시작번호부터 페이지 당 보여질 목록수 만큼 데이터 구하는 쿼리 작성
+          // limit 몇번부터, 몇 개
+          $sql = "select * from lodging limit $start, $list_num;";
+          // echo $sql;
+          /* exit; */
+
+          // DB에 데이터 전송
+          $result = mysqli_query($dbcon, $sql);
+
+          // DB에서 데이터 가져오기
+          // pager : 글번호
+          $i = $start + 1;
+          while($array = mysqli_fetch_array($result)){
+        ?>
         <div class="ldg_card">
-          <img src="../../images/search_room_img01.png" alt="검색된숙소이미지1">
+          <img src="../../images/search_room_img01.png" alt="숙소이미지">
           <ul class="ldg_card_menu">
             <li><a href="../reservation/partner_reservation_page.php" class="menu_btn btn_hover">예약관리</a></li>
-            <li><a href="edit_room_page.php" class="menu_btn btn_hover">숙소관리</a></li>
+            <li><a href="edit_room_page.php?ldg_idx=<?php echo $array["ldg_idx"]; ?>" class="menu_btn btn_hover">숙소관리</a></li>
             <li><a href="#" class="menu_btn btn_hover">후기관리</a></li>
           </ul>
           <div class="ldg_card_left">
             <div class="ldg_card_top">
-              <p class="lodging_name">런던스테이</p>
+              <p id="ldg_name" class="lodging_name"><?php echo $array["ldg_name"]; ?></p>
               <p class="room_type">도미토리 · 개인실</p>
             </div>
             <div class="ldg_card_bot">
@@ -81,6 +145,10 @@
             <p class="ldg_card_price">90,900 원~</p>
           </div>
         </div>
+        <?php
+                $i++;
+            }; 
+        ?>
       </div>
     </main>
 
